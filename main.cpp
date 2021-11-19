307ea81efc5a51337b3fc98a3860a443ebeb39c3
minho2618 <minho2618@naver.com>
2021-11-20 01:32:57 +0900

    21.11.19 현재 디렉토리명 구현

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
 #include <ncurses.h>
+#include <stack>
+#include <string>
+#include <unistd.h>
+
+#define MAX_PATH_LEN 1024
 
 using namespace std;
 
+// 프로그램을 실행한 위치의 디렉토리의 주소를 알아냄
+stack<string> currentDir();
+
+// 디렉토리 스택 표시용
+string showDir(stack<string> stc);
+
+// 디렉토리 내 파일 표시용
 void showFile(WINDOW *win);
 
 int main() {
@@ -10,7 +23,13 @@ int main() {
     // command : 명령어 및 현재 상태 안내바
     // fileShow : 파일 미리 보기
     WINDOW *dirLocal, *fileIn1, *command, *fileIn2, *fileShow;
+
     char press;
+
+    // 디렉토리 저장용 스택
+    stack<string> dirLocation;
+
+    dirLocation = currentDir();
 
     initscr();
 
@@ -39,7 +58,7 @@ int main() {
     wbkgd(fileIn2, COLOR_PAIR(1));
     wbkgd(command, COLOR_PAIR(2));
 
-    wprintw(dirLocal, "Current Dir : /home/lmh");
+    wprintw(dirLocal, showDir(dirLocation).c_str());
     showFile(fileIn1);
     showFile(fileIn2);
     wprintw(command, "F1: Copy F2: Move F3: Delete F4: Rename F5: New File F6: "
@@ -79,4 +98,53 @@ void showFile(WINDOW *win) {
     mvwprintw(win, 5, 1, "test.txt");
     mvwprintw(win, 6, 1, "text.txt");
     mvwprintw(win, 7, 1, "project.c");
+}
+
+stack<string> currentDir() {
+    stack<string> stc;
+
+    char dirChar[MAX_PATH_LEN + 1] = {
+        '\0',
+    };
+
+    if (getcwd(dirChar, MAX_PATH_LEN) == NULL) {
+        perror("getcwd error!");
+        exit(-1);
+    }
+
+    string dirStr(dirChar);
+
+    while (true) {
+        int cursor = 0;
+
+        if (dirStr.empty())
+            break;
+
+        cursor = dirStr.find("/", 1);
+        cout << cursor << endl;
+
+        stc.push(dirStr.substr(1, cursor - 1));
+        stc.top();
+
+        dirStr.erase(0, cursor);
+        cout << dirStr << endl;
+    }
+
+    return stc;
+}
+
+string showDir(stack<string> stc) {
+    stack<string> tmpStr = stc;
+    string dirStr;
+
+    while (true) {
+        if (tmpStr.empty())
+            break;
+
+        dirStr.insert(0, tmpStr.top());
+        dirStr.insert(0, "/");
+        tmpStr.pop();
+    }
+
+    return dirStr;
 }
